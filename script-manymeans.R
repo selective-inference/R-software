50f05ac8d691083633ba38758fa0e5fb8c4983c0
tibshirani <tibs@stanford.edu>
2015-05-05 15:29:43 -0700

    rob

---
+++ script-manymeans.R
@@ -0,0 +1,12 @@
+ library(selectiveInference,lib.loc="mylib")
+
+set.seed(12345)
+
+n = 100 # sample size
+signal = 3 # signal size
+mu = c(rep(signal, floor (n/5)), rep(0, n-floor(n/5))) # 20% of elements get the signal; rest 0
+mu=sample(mu)
+y = mu + rnorm (n, 0, 1)
+
+mmObj = manyMeans(y, bh.q=0.1)
+
