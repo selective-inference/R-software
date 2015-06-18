c5fedcb56fd435999a273ba33b1557ba50b66b47
tibshirani <tibs@stanford.edu>
2015-06-18 15:59:12 -0700

    rob modified script

--- josh/test_fstep.R
+++ josh/test_fstep.R
@@ -30,3 +30,29 @@ sparsity <- 0
 snr <- 1
 
 pvals <- replicate(5, instance(n, p, sparsity, snr, index))
+
+
+
+##  rob's script
+set.seed(30)
+n=20
+p=10
+index <- sort(rep(1:(p/2),2 ))
+
+    x <- matrix(rnorm(n*p), nrow=n)
+x=scale(X,T,T)
+nsteps=4
+pvals=matrix(NA,nsim,nsteps)
+nsim=100
+for(ii in 1:nsim){
+    cat(ii)
+    y=rnorm(n)
+ fit <- fstep(x, y, index, steps = nsteps)
+  pvals[ii,] <- interval.fstep(fit, x, y, index)
+}
+par(mfrow=c(2,2))
+
+for(k in 1:4)
+    plot((1:nsim)/nsim,sort(pvals[,k]))
+abline(0,1)
+}
