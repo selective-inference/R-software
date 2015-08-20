ddb4ad361e6281d5777fcba5da03283ca8c79611
tibs <tibs@stanford.edu>
2015-08-20 09:24:39 -0700

    rob

--- debug/test.fixed.R
+++ debug/test.fixed.R
@@ -1,8 +1,8 @@
-library(selectiveInference)
-#library(selectiveInference,lib.loc="/Users/tibs/dropbox/git/R/mylib")
+#library(selectiveInference)
+library(selectiveInference,lib.loc="/Users/tibs/dropbox/git/R/mylib")
 
 #options(error=dump.frames)
-attach("/Users/tibs/dropbox/PAPERS/lasso/lasso3/.RData")
+#attach("/Users/tibs/dropbox/PAPERS/lasso/lasso3/.RData")
 
 set.seed(133)
 n=100
@@ -40,39 +40,43 @@ critf=function(b,lam,x,y){
 ##check coverage
 set.seed(3)
 
-n=15
+n=50
 p=10
-sigma=2.3
+sigma=2
 
 x=matrix(rnorm(n*p),n,p)
-x=scale(x,T,T)/sqrt(n-1)
+#x=scale(x,T,T)/sqrt(n-1)    #try with and without standardization
 
-beta=c(rep(2,5),rep(0,p-5))
-     lambda = 1
-nsim=1
+beta=c(5,4,3,2,1,rep(0,p-5))
+
+nsim=100
 seeds=sample(1:9999,size=nsim)
-
+pv=rep(NA,nsim)
 ci=matrix(NA,nsim,2)
 btrue=rep(NA,nsim)
 for(ii in 1:nsim){
     cat(ii)
     set.seed(seeds[ii])
     mu=x%*%beta
-y=mu+sigma*rnorm(n)
+   y=mu+sigma*rnorm(n)
+    y=y-mean(y)  
+   gfit=glmnet(x,y,standardize=F,lambda.min.ratio=1e-9)
+   ilam=trunc(length(gfit$lam)/4)
+    lambda=gfit$lam[ilam]*n
+     bhat = predict(gfit, s=lambda/n,type="coef",exact=F)[-1]
 
-y=y-mean(y)
-    
-
-    
-gfit=glmnet(x,y,standardize=F,lambda.min.ratio=1e-9)
-
-     bhat = predict(gfit, s=lambda/n,type="coef",exact=T)[-1]
-#bhat2=lasso2lam(x,y,lambda,int=F,stand=F)
- junk= fixedLassoInf(x,y,bhat,lambda,sigma=sigma)
-ci[ii,]=junk$ci[3,]
+     junk= fixedLassoInf(x,y,bhat,lambda,sigma=sigma)
+    pv[ii]=junk$pv[1]
+   # oo=junk$pred # for old package
+    oo=junk$var   # for new package
+     btrue[ii]=lsfit(x[,oo],mu)$coef[2]
+     ci[ii,]=junk$ci[1,]
 }
 
-sum(ci[,1]< btrue & ci[,]> btrue)/nsim
+sum(ci[,1]> btrue)
+sum(ci[,2]< btrue)
+
+
 
 
 ## BIG example
