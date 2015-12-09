f2715b47af25b60946f0368e215866bca3c9f155
Joshua Loftus <joftius@stanford.edu>
2015-12-09 11:52:56 -0800

    Just running simulations

--- forLater/josh/sim.cv.R
+++ forLater/josh/sim.cv.R
@@ -5,12 +5,12 @@ source("../../selectiveInference/R/funs.quadratic.R")
 source("../../selectiveInference/R/funs.common.R")
 
 set.seed(1)
-niters <- 400
-n <- 50
-p <- 100
+niters <- 500
+n <- 100
+p <- 200
 maxsteps <- 10
 sparsity <- 5
-snr <- 2
+snr <- 1
 nfolds <- 5
 
 instance <- function(n, p, sparsity, snr, maxsteps, nfolds) {
@@ -24,11 +24,14 @@ instance <- function(n, p, sparsity, snr, maxsteps, nfolds) {
       y <- y + x %*% beta
     }
 
-    fit <- cvfs(x, y, maxsteps=maxsteps, nfolds=nfolds)
-    fit2 <- groupfs(x, y, index = 1:p, maxsteps = attr(fit, "maxsteps"))
+    fit <- cvfs(x, y, maxsteps=maxsteps, sigma=1, nfolds=nfolds)
+    fit2 <- groupfs(x, y, index = 1:p, maxsteps = attr(fit, "maxsteps"), sigma = 1)
     pvals <- groupfsInf(fit, verbose=T)
     pv2 <- groupfsInf(fit2, verbose=T)
-    return(list(variable = fit$action, pvals = pvals$pv, var2 = fit2$action, pvals2 = pv2$pv))
+    Y <- y - mean(y)
+    cols <- which(1:p %in% fit$action)
+    pv3 <- summary(lm(Y~x[, cols]-1))$coefficients[,4]
+    return(list(variable = fit$action, pvals = pvals$pv, var2 = fit2$action, pvals2 = pv2$pv, pvals3 = pv3[order(fit$action)]))
 }
 
 time <- system.time({
@@ -39,12 +42,13 @@ pvals <- do.call(c, list(output[2,]))
 vars <- do.call(c, list(output[1,]))
 vars2 <- do.call(c, list(output[3,]))
 pvals2 <- do.call(c, list(output[4,]))
+pvals3 <- do.call(c, list(output[5,]))
 
-save(pvals, vars, vars2, pvals2, file = paste0(
+save(pvals, vars, vars2, pvals2, pvals3, file = paste0(
                       "results_cv_n", n,
                       "_p", p,
                       "_sparsity", sparsity,
                       "_snr", snr,
-                      "_comparison.RData"))
+                      "_comparison_TC.RData"))
 
 print(time)
