1341329d959b62bf98ca74f3e6f56b4d2e8f1e95
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-08-18 10:46:39 -0700

    more renaming

--- selectiveInference/src/debias.h
+++ selectiveInference/src/debias.h
@@ -3,16 +3,16 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-int find_one_row_(double *Sigma,          /* A covariance matrix: X^TX/n */
-		  double *Sigma_diag,     /* Diagonal entry of covariance matrix */
-		  double *gradient_ptr,   /* Current gradient of quadratic loss */
+int find_one_row_(double *Sigma_ptr,          /* A covariance matrix: X^TX/n */
+		  double *Sigma_ptr_ptr,     /* Diagonal entry of covariance matrix */
+		  double *gradient_ptr,       /* Current gradient of quadratic loss */
 		  int *ever_active_ptr,       /* Ever active set: 0-based */ 
-		  int *nactive_ptr,       /* Size of ever active set */
-		  int nrow,               /* How many rows in Sigma */
-		  double bound,           /* feasibility parameter */
-		  double *theta,          /* current value */
-		  int maxiter,            /* how many iterations */
-		  int row);               /* which coordinate to update: 0-based */
+		  int *nactive_ptr,           /* Size of ever active set */
+		  int nrow,                   /* How many rows in Sigma */
+		  double bound,               /* feasibility parameter */
+		  double *theta,              /* current value */
+		  int maxiter,                /* how many iterations */
+		  int row);                   /* which coordinate to update: 0-based */
 
 int check_KKT(double *theta,           /* current theta */
 	      double *gradient_ptr,    /* Current gradient of quadratic loss */
