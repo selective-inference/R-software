4c3acfac7721b1e30108906a076479b5bc8eca5b
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-08-18 12:30:59 -0700

    BF: fixing function signatures

--- selectiveInference/src/debias.h
+++ selectiveInference/src/debias.h
@@ -12,13 +12,11 @@ int find_one_row_(double *Sigma_ptr,          /* A covariance matrix: X^TX/n */
 		  int nrow,                   /* How many rows in Sigma */
 		  double bound,               /* feasibility parameter */
 		  double *theta,              /* current value */
-		  int maxiter) //,                /* how many iterations */
-// int row);                   /* which coordinate to update: 0-based */
+		  int maxiter);
 
 int check_KKT(double *theta,           /* current theta */
 	      double *gradient_ptr,    /* Current gradient of quadratic loss */
 	      int nrow,                /* how many rows in Sigma */
-	      // int row,                 /* which row: 0-based */
 	      double bound);           /* Lagrange multipler for \ell_1 */
 
 
