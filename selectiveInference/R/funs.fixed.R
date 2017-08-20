f12090c08367da6780fb01f492eb64efffb06f3a
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-08-20 15:16:24 -0700

    passing tol as argument; both solvers agree

--- selectiveInference/R/funs.fixed.R
+++ selectiveInference/R/funs.fixed.R
@@ -338,7 +338,7 @@ InverseLinfty <- function(sigma, n, e, resol=1.2, mu=NULL, maxiter=50, threshold
   return(M)
 }
 
-InverseLinftyOneRow <- function (Sigma, i, mu, maxiter=50, soln_result=NULL) {
+InverseLinftyOneRow <- function (Sigma, i, mu, maxiter=50, soln_result=NULL, kkt_tol=1.e-6, objective_tol=1.e-6) {
 
   # If soln_result is not Null, it is used as a warm start.
   # It should be a list
@@ -365,10 +365,18 @@ InverseLinftyOneRow <- function (Sigma, i, mu, maxiter=50, soln_result=NULL) {
      linear_func = soln_result$linear_func
   }
 
-  result = find_one_row_debiasingM(Sigma, i, mu, maxiter, 0 * soln, gradient, ever_active, nactive) # C function uses 0-based indexing
-  #result1 = solve_QP(Sigma, mu, maxiter, soln, linear_func, gradient, ever_active, nactive) 
-  #print("close?")
-  #print(c(sqrt(sum((result1$soln-result$soln)^2)/sum(result$soln^2)), sum(result$soln^2)))
+  soln1 = rep(0, p)
+  gradient1 = rep(0, p)
+  ever_active1 = rep(0, p)
+  ever_active1[1] = i-1
+  nactive1 = as.integer(1)
+  result1 = find_one_row_debiasingM(Sigma, i-1, mu, maxiter, soln1, gradient1, ever_active1, nactive1, kkt_tol, objective_tol) # C function uses 0-based indexing
+  result = solve_QP(Sigma, mu, maxiter, soln, linear_func, gradient, ever_active, nactive, kkt_tol, objective_tol) 
+  print("close?")
+  print(c(sqrt(sum((result1$soln-result$soln)^2)/sum(result$soln^2)), sum(result$soln^2)))
+  print(c(result1$iter, result$iter, sum(result1$soln^2)))
+
+  #result = find_one_row_debiasingM(Sigma, i-1, mu, maxiter, soln, gradient, ever_active, nactive, kkt_tol, objective_tol) # C function uses 0-based indexing
 
   # Check feasibility
 
