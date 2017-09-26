3dcb18f1386eb944bb7d663a4abe40d6eaf0145c
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-09-26 14:55:18 -0700

    test running now -- no segfault?

--- test.R
+++ test.R
@@ -2,22 +2,28 @@ set.seed(43)
 
     n = 100                                                                                                                                                                              
     p = 200                                                                                                                                                                              
-    lam = 10                                                                                                                                                                             
+    lam = 0.1                                                                                                                                                                             
     X = matrix(rnorm(n*p), n, p)                                                                                                                                                         
     Y = rnorm(n)                                                                                                                                                                         
     library(selectiveInference)                                                                                                                                                          
     p = ncol(X)                                                                                                                                                                          
     soln_R = rep(0, p)                                                                                                                                                                   
-    grad = -t(X) %*% Y                                                                                                                                                                   
+    grad = -t(X) %*% Y / n                                                                                                                                                                   
     ever_active = c(1, rep(0, p-1))                                                                                                                                                      
     nactive = as.integer(1)                                                                                                                                                              
     kkt_tol = 1.e-12                                                                                                                                                                     
     objective_tol = 1.e-12                                                                                                                                                               
     maxiter = 500                                                                                                                                                                        
-    Xtheta = rep(0, n)                                                                                                                                                                   
-    soln_R = selectiveInference:::solve_QP(t(X) %*% X, lam, maxiter, soln_R, -t(X) %*% Y, grad, ever_active, nactive, kkt_tol, objective_tol, p)$soln                                    
+    soln_R = selectiveInference:::solve_QP(t(X) %*% X / n, lam, maxiter, soln_R, -t(X) %*% Y / n, grad, ever_active, nactive, kkt_tol, objective_tol, p)$soln                                    
     print(soln_R)                                                                                                                                                                        
+    Xtheta = rep(0, n)                                                                                                                                                                   
+    nactive = as.integer(1)
+    ever_active = c(1, rep(0, p-1))                                                                                                                                                      
+    soln_R = rep(0, p)
+    grad = - t(X) %*% Y / n
     # test wide solver                                                                                                                                                                   
-                                                                                                                                                                                       
-    soln_R_wide = selectiveInference:::solve_QP_wide(X, lam, maxiter, soln_R, -t(X) %*% Y, grad, Xtheta, ever_active, nactive, kkt_tol, objective_tol, p)
+    soln_R_wide = selectiveInference:::solve_QP_wide(X, lam, maxiter, soln_R, -t(X) %*% Y / n, grad, Xtheta, ever_active, nactive, kkt_tol, objective_tol, p)
     print(soln_R_wide)
+    print(soln_R)
+    print(soln_R_wide$soln)
+    print(soln_R_wide$soln - soln_R)
