c175a8b192b56a1ea1ad9fabe688461f459f770f
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-10-28 12:03:00 -0700

    vectorized densities evaluate OK -- need to check results

--- tests/randomized/test_randomized.R
+++ tests/randomized/test_randomized.R
@@ -1,6 +1,6 @@
 library(selectiveInference)
 
-test = function() {
+smoke_test = function() {
 
     n = 100; p = 50
     X = matrix(rnorm(n * p), n, p)
@@ -10,6 +10,27 @@ test = function() {
     ridge_term = .1 / sqrt(n)
     selectiveInference:::randomizedLASSO(X, y, lam, noise_scale, ridge_term)
 }
+A = smoke_test()
 
-A=test()
-#print(test())
+density_test = function() {
+
+    random_lasso = smoke_test()
+    p = nrow(random_lasso$internal_transform$linear_term)
+    internal_state = matrix(rnorm(p * 20), p, 20)
+    optimization_state = matrix(rnorm(p * 20), p, 20)
+    offset = rnorm(p)
+
+    selectiveInference:::log_density_gaussian_(10.,
+                                               random_lasso$internal_transform$linear_term,
+                                               internal_state,
+                                               random_lasso$optimization_transform$linear_term,
+					       optimization_state,
+                                               offset)
+
+    selectiveInference:::log_density_gaussian_conditional_(10.,
+                                                           random_lasso$optimization_transform$linear_term,
+                    				           optimization_state,
+                                                           offset)
+}
+
+density_test()
