8516cad5ba7d28ec33691f24aa2a0da51f71441c
Jonathan Taylor <jonathan.taylor@stanford.edu>
2017-11-14 16:21:19 -0800

    simple problem seems to be working -- will make plot

--- tests/randomized/test_MLE.R
+++ tests/randomized/test_MLE.R
@@ -1,11 +1,11 @@
 library(selectiveInference)
 
-simple_problem = function(target_observed=2, threshold=2, randomization_scale=1.) {
+simple_problem = function(target_observed=-3., threshold=2., randomization_scale=1.) {
     # Simple problem: randomizaiton of sd 1 and thresholded at 2 (default args)
 
     target_observed = as.numeric(target_observed)
-    target_transform = list(linear_term=-diag(1), offset=c(0.))
-    opt_transform = list(linear_term=diag(1), offset=c(threshold*1.))
+    target_transform = list(linear_term=-diag(1), offset_term=c(0.))
+    opt_transform = list(linear_term=diag(1), offset_term=c(threshold*1.))
     feasible_point = c(1.)
     randomizer_precision = diag(1. / randomization_scale^2)
     target_cov = diag(1)
