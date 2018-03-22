44e2c508b3d800c12dcbe329c72adbab9005af74
Jelena Markovic <jelenam@stanford.edu>
2018-03-21 21:37:58 -0700

    cluster again

--- tests/liu_high_dim_full/cluster.R
+++ tests/liu_high_dim_full/cluster.R
@@ -5,8 +5,6 @@ args = commandArgs(trailingOnly=TRUE)
 seed = round(as.numeric(args[1]))
 
 outdir = "/scratch/users/jelenam/full/"
-outdir = paste(outdir, method, "_n", n, "p", p,"_", regen, sep='')
-dir.create(outdir)
 outfile = file.path(outdir, paste(sep="","list_result_", toString(seed), ".rda"))
 
 test_liu_full(seed, outfile)
